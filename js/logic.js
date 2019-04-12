e0d84b4381a4d9e67cc655df163d5432029bf9b4
Ethan <erogers999@gmail.com>
2019-04-11 22:59:13 -0400

    design test

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,28 @@
 $(document).ready(function() {
-    
-    $(".box").click(function() {
+
+    // Box Click Functions 
+    $("#box-1").click(function() {
+        //Do something
+    });
+    $("#box-2").click(function() {
+        //Do something
+    });
+    $("#box-3").click(function() {
+        //Do something
+    });
+    $("#box-4").click(function() {
+        //Do something
+    });
+    $("#box-5").click(function() {
+        //Do something
+    });
+    $("#box-6").click(function() {
+        //Do something
+    });
+    $("#box-7").click(function() {
+        //Do something
+    });
+    $("#box-8").click(function() {
+        //Do something
     });
 });
